d056277dfd545fabca8b7d9ae863832148658b1f
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-06-03 17:54:01 +0700

    menambahkan code untuk menampilkan nilai x&y

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -6,10 +6,18 @@ class AbstraksiKlas {
     string x,y;
 
     public:
+    // method untuk mengisi nilai
+    //private number
     void setX(string a, string b) {
         x = a;
         y = b;
     }
-    
+
+    //menampilkan nilai
+    void display(){
+        cout << "x: " << x << endl;
+        cout << "y: " << y << endl;
+    }
+
 
 };
